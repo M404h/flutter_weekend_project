58716ad50a48c3367647416aa618e4d272a69117
BM <drhouse_10@hotmail.com>
2022-11-18 17:53:52 +0300

    travel -

--- lib/Models/Classes/travel.dart
+++ lib/Models/Classes/travel.dart
@@ -1,6 +1,15 @@
 class Travel {
   final String country;
-  final Map<String, String> expenses;
+  final String? resturantExpenses;
+  final String? hotelExpenses;
+  final String? enterntainmentExpenses;
+  final String? flightExpenses;
 
-  Travel({required this.country, required this.expenses});
+  Travel({
+    required this.country,
+    this.resturantExpenses,
+    this.enterntainmentExpenses,
+    this.hotelExpenses,
+    this.flightExpenses,
+  });
 }
